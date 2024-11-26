93446c77818574bce5d9ffe5fba509d138a521ed
Edward <78192184+F8TGT@users.noreply.github.com>
2024-11-26 08:08:44 -0600

    → Created the db connection and model for campgrounds.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+const dbConnection = "mongodb://localhost:27017/yelp-camp";
+mongoose.connect(dbConnection);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Database connected successfully.'));
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,4 +19,10 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp);
+})
+
+    app.listen(3000, () => console.log('Server running on port 3000'));
