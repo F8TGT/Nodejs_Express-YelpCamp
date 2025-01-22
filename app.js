da805f18b7a1045f824f68e8df09ee960a9447e7
Edward <78192184+F8TGT@users.noreply.github.com>
2025-01-22 07:53:15 -0600

    → The "Register," "Login" options will only appear if the user is not logged in and the "Logout" option will appear if the user is logged in.

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ passport.deserializeUser(async (id, done) => {
 });
 
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
