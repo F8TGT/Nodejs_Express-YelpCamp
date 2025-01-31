5edf36856fa6d0b5ffa9898ce7f4aff94e1ca56f
Edward <78192184+F8TGT@users.noreply.github.com>
2025-01-31 08:03:48 -0600

    → Added the author to the review schema. → Hide the add a review section, and it will only display if a user is logged in. → Added the isLoggedIn middleware to the review to check if the user is logged in.

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const {validateReview} = require('../middleware.js');
+const {validateReview, isLoggedIn} = require('../middleware.js');
 const catchAsync = require('../utils/catchAsync');
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user.id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
