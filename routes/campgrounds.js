bc1d5a9cf10535dcac6ea4db774b8267bb59838e
Edward <78192184+F8TGT@users.noreply.github.com>
2025-02-14 07:54:14 -0600

    → Set up the upload image to the cloud database.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,14 +4,15 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const {storage} = require('../cloudinary');
+const upload = multer({storage});
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
     // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
     .post(upload.array('image'), (req, res) => {
-        console.log(req.body, req.file);
+        console.log(req.body, req.files);
         res.send('It worked!');
     })
 
