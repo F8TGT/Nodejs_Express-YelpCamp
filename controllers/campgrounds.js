d498c2c794e3290702200009f6676e48e717ebd4
Edward <78192184+F8TGT@users.noreply.github.com>
2025-02-26 08:11:53 -0600

    → Forward geocode the location of the new campground.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,7 @@
 const Campground = require('../models/campground');
 const {cloudinary} = require('../cloudinary');
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 
 
 module.exports.index = async function (req, res) {
@@ -12,13 +14,17 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res) => {
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, {limit: 1});
     const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    campground.author = req.user._id;
-    await campground.save();
-    console.log(campground);
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
+    campground.geometry = geoData.features[0].geometry;
+    res.send(campground.geometry.coordinates);
+    // const campground = new Campground(req.body.campground);
+    // campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
+    // campground.author = req.user._id;
+    // await campground.save();
+    // console.log(campground);
+    // req.flash('success', 'Successfully made a new campground!');
+    // res.redirect(`/campgrounds/${campground._id}`);
 }
 
 module.exports.showCampground = async (req, res) => {
